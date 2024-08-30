155f2ead65e5a362fc23f95e11e6b90fd0f51d35
Redish101 <i@redish101.top>
2024-08-30 08:50:38 +0800

    chore: 优化加载动画

--- src/app/article/[slug]/loading.tsx
+++ src/app/article/[slug]/loading.tsx
@@ -1,12 +1,18 @@
 import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import "./loading.css";
 
 export default function Loading() {
   return (
-    <div className="my-16 flex flex-col gap-4">
-      <Skeleton height={60} /> {/* 替换标题 */}
-      <Skeleton height={25} /> {/* 替换日期 */}
-      <Skeleton count={5} height={50} /> {/* 替换正文部分 */}
+    <div className="transition-all">
+      <div className="my-16 flex flex-col gap-4">
+        <h1 className="text-3xl font-medium leading-normal">
+          <Skeleton width="100%" height={60} />
+        </h1>
+        <div className="font-normal text-slate-600 dark:text-slate-400">
+          <Skeleton width="100%" height={24} />
+        </div>
+      </div>
+      <Skeleton width="100%" height={320} />
     </div>
   );
 }
