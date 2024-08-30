858257957aaa853d93f4c49e9e9b4ab17a5b077c
Redish101 <i@redish101.top>
2024-08-30 09:47:24 +0800

    feat: 支持GitHub风格markdown渲染

--- src/core/render.ts
+++ src/core/render.ts
@@ -1,5 +1,6 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import rehypeShiki from "@shikijs/rehype";
@@ -8,6 +9,7 @@ import { cache } from "react";
 const render = cache(async (content: string) => {
   const processor = unified()
     .use(remarkParse)
+    .use(remarkGfm)
     .use(remarkRehype)
     .use(rehypeShiki, { theme: "github-dark" })
     .use(rehypeStringify);
