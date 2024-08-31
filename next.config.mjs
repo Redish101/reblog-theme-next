9374d36492a6972f53a37707c3898821eac540d5
Redish101 <i@redish101.top>
2024-08-31 09:37:51 +0800

    fix: 修复配置错误

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 import { withSentryConfig } from "@sentry/nextjs";
-import { getConfig } from "@/core/config";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-/** @type {import('@/core/config').SentryConfig} */
-const config = getConfig().sentry;
-
 export default withSentryConfig(nextConfig, {
-  org: config.org,
-  project: config.project,
+  org: "redish101",
+  project: "redish101-blog",
   authToken: process.env.SENTRY_AUTH_TOKEN,
   silent: false,
 });
