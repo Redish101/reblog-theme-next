b7c02d2efc96bd345813f5541431537ee0d3e6ad
Redish101 <i@redish101.top>
2024-09-15 07:20:25 +0800

    fix: 修复了开发环境下使用sentry的问题

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,11 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(nextConfig, {
+const config = process.env.NODE_ENV === "production" ? withSentryConfig(nextConfig, {
   org: "redish101",
   project: "redish101-blog",
   authToken: process.env.SENTRY_AUTH_TOKEN,
   silent: false,
-});
+}) : nextConfig;
+
+export default config;
