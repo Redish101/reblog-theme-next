96d8e70c476411d474d9b1da14078f3bd5328d4c
Redish101 <i@redish101.top>
2024-09-15 09:12:10 +0800

    remove: 移除cdn'支持

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
-import nextBuildId from "next-build-id";
 import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  generateBuildId: () => nextBuildId({ dir: import.meta.dirname }),
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://cdn1.tianli0.top/gh/redish101/reblog-theme-next@static-resources"
-      : undefined,
-};
+const nextConfig = {};
 
 const config =
   process.env.NODE_ENV === "production"
