015feb477ab34bfd6bf2b0cf404152e903900df1
Redish101 <i@redish101.top>
2024-10-29 22:16:26 +0800

    fix: 移除 sitemap

--- src/app/(blog)/sitemap.ts
+++ src/app/(blog)/sitemap.ts
@@ -2,27 +2,27 @@ import getThemeKit from "@/libs/themekit";
 import getSiteInfo from "@/utils/siteInfo";
 import type { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const themekit = getThemeKit();
-  const site = await getSiteInfo();
-  const articles = await themekit.getArticleList({
-    pageIndex: 1,
-    pageSize: Number.MAX_SAFE_INTEGER,
-  });
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const themekit = getThemeKit();
+//   const site = await getSiteInfo();
+//   const articles = await themekit.getArticleList({
+//     pageIndex: 1,
+//     pageSize: Number.MAX_SAFE_INTEGER,
+//   });
 
-  const articlesSitemap: MetadataRoute.Sitemap =
-    articles?.articles.map((article) => ({
-      url: `${site?.url!}/article/${article.slug}`,
-      changeFrequency: "daily",
-      priority: 0.8,
-    })) || [];
+//   const articlesSitemap: MetadataRoute.Sitemap =
+//     articles?.articles.map((article) => ({
+//       url: `${site?.url!}/article/${article.slug}`,
+//       changeFrequency: "daily",
+//       priority: 0.8,
+//     })) || [];
 
-  return [
-    {
-      url: site?.url!,
-      changeFrequency: "daily",
-      priority: 1,
-    },
-    ...articlesSitemap,
-  ];
-}
+//   return [
+//     {
+//       url: site?.url!,
+//       changeFrequency: "daily",
+//       priority: 1,
+//     },
+//     ...articlesSitemap,
+//   ];
+// }
