2576a380e236ac2b67ca9e418cc05b044a8ef24f
Redish101 <i@redish101.top>
2024-10-30 21:54:12 +0800

    fix: 破防了,不修了,杀掉灯箱

--- src/components/Markdown/renderers/Image.tsx
+++ src/components/Markdown/renderers/Image.tsx
@@ -1,31 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 
-import { Fancybox as NativeFancybox } from "@fancyapps/ui";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
+// import { Fancybox as NativeFancybox } from "@fancyapps/ui";
+// import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
+// FIXME: Error: Cannot access default.props on the server. You cannot dot into a client module from a server component. You can only pass the imported name through.
 const MImage: React.FC<{ src: string; alt?: string }> = (props) => {
-  const imgRef = useRef(null);
+  // const imgRef = useRef(null);
 
-  useEffect(() => {
-    const container = imgRef.current;
+  // useEffect(() => {
+  //   const container = imgRef.current;
 
-    const delegate = "[data-fancybox]";
+  //   const delegate = "[data-fancybox]";
 
-    NativeFancybox.bind(container, delegate, { Carousel: { infinite: true } });
+  //   NativeFancybox.bind(container, delegate, { Carousel: { infinite: true } });
 
-    return () => {
-      NativeFancybox.unbind(container);
-      NativeFancybox.close();
-    };
-  }, [imgRef]);
+  //   return () => {
+  //     NativeFancybox.unbind(container);
+  //     NativeFancybox.close();
+  //   };
+  // }, [imgRef]);
 
   return (
-    <figure className="my-8" ref={imgRef}>
-      <a data-fancybox="gallery" href={props.src}>
-        <img src={props.src} alt={props.alt || ""} className="rounded-md" />
-      </a>
+    <figure className="my-8">
+      <img src={props.src} alt={props.alt || ""} className="rounded-md" />
       {props.alt && (
         <figcaption className="mt-4 text-slate-600 text-sm text-center">
           {props.alt}
