6208a1c3bd54ce583495ce11215183bbb1752f56
Redish101 <i@redish101.top>
2024-11-03 18:21:22 +0800

    fix: 超链接换行时的错误样式

--- src/components/Markdown/renderers/Link.tsx
+++ src/components/Markdown/renderers/Link.tsx
@@ -1,5 +1,4 @@
 import clsx from "clsx";
-import Link from "next/link";
 
 const MLink: React.FC<{
   href: string;
@@ -10,9 +9,9 @@ const MLink: React.FC<{
     href={props.href}
     target="_blank"
     className={clsx(
-      "relative",
-      "after:absolute after:left-0 after:right-0 after:bottom-[-.05em] after:z-[-1] after:h-[.425rem] after:content-['']",
-      "after:bg-blue-200 after:inline-block",
+      "inline-block relative break-words max-w-full",
+      "after:absolute after:left-0 after:right-0 after:bottom-[-.05em] after:z-[-1] after:h-1/4 after:content-['']",
+      "after:bg-blue-200",
       "dark:after:bg-blue-800",
       "hover:after:bg-blue-500 hover:after:h-[.1em]",
       "after:transition-all after:duration-200 after:ease-out",
