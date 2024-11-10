4a839542b8c078ad490a4b05641906f756b229cc
Redish101 <i@redish101.top>
2024-11-10 10:24:13 +0800

    feat: 页脚提交信息

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,14 @@
-export default function Footer() {
+import getLastestCommit from "@/libs/git";
+
+export default async function Footer() {
+  const commit = await getLastestCommit();
   return (
     <footer className="py-4 text-center text-sm text-slate-600">
-      Powered by reblog
+      <div>Powered by reblog</div>
+      <div>
+        {commit.msg}(
+        <span className="uppercase">{commit.hash?.substring(0, 7)}</span>)
+      </div>
     </footer>
   );
 }
