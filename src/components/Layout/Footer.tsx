ba95b4aeeb6ac6758e6421cb01ea01603b5f1bb5
Redish101 <i@redish101.top>
2024-11-10 10:42:41 +0800

    Revert "feat: 页脚提交信息"
    This reverts commit 4a839542b8c078ad490a4b05641906f756b229cc.

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,14 +1,7 @@
-import getLastestCommit from "@/libs/git";
-
-export default async function Footer() {
-  const commit = await getLastestCommit();
+export default function Footer() {
   return (
     <footer className="py-4 text-center text-sm text-slate-600">
-      <div>Powered by reblog</div>
-      <div>
-        {commit.msg}(
-        <span className="uppercase">{commit.hash?.substring(0, 7)}</span>)
-      </div>
+      Powered by reblog
     </footer>
   );
 }
