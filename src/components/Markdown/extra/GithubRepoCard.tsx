1837474901fb24a1155f2c00e6e17f8ae7fd670b
Redish101 <i@redish101.top>
2024-11-17 09:25:48 +0800

    fix: GithubRepoCard可选无简介

--- src/components/Markdown/extra/GithubRepoCard.tsx
+++ src/components/Markdown/extra/GithubRepoCard.tsx
@@ -1,12 +1,10 @@
 import clsx from "clsx";
-import { getRepo } from "@/libs/github";
 import Link from "next/link";
-import { Suspense } from "react";
 
 interface GithubRepoCardProps {
   owner: string;
   repo: string;
-  desc: string
+  desc?: string;
 }
 
 // FIXME: 调用GitHub API时意外的Connection close问题
@@ -23,7 +21,7 @@ const GithubRepoCard: React.FC<GithubRepoCardProps> = (props) => {
         "max-w-96 bg-slate-100 mx-auto rounded-lg border-slate-300 border p-4 my-6 flex gap-3",
         "dark:bg-slate-800 dark:border-slate-700",
         "md:hover:shadow-lg md:hover:-translate-y-1 transition-all ease-out hover:shadow-xl hover:-translate-y-2",
-        "active:shadow-none active:translate-y-0",
+        "active:shadow-none active:translate-y-0"
       )}
       href={`https://github.com/${props.owner}/${props.repo}`}
     >
@@ -34,12 +32,16 @@ const GithubRepoCard: React.FC<GithubRepoCardProps> = (props) => {
         <div className="font-bold text-lg">
           {props.owner}/{props.repo}
         </div>
-        <div className="font-light text-sm text-slate-500">
-          {/* <Suspense fallback={<>正在加载仓库信息</>}>
-            <RepoDescription owner={props.owner} repo={props.repo} />
-          </Suspense> */}
-          {props.desc}
-        </div>
+        {props.desc ? (
+          <div className="font-light text-sm text-slate-500">
+            {/* <Suspense fallback={<>正在加载仓库信息</>}>
+                  <RepoDescription owner={props.owner} repo={props.repo} />
+                </Suspense> */}
+            {props.desc}
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </Link>
   );
