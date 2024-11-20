1e77de166ecb060196079173d2e5255cc4d7e04d
Redish101 <i@redish101.top>
2024-11-20 21:52:25 +0800

    chore: 优化版权卡片文案

--- src/components/CopyrightCard.tsx
+++ src/components/CopyrightCard.tsx
@@ -13,7 +13,15 @@ const CopyrightCard: React.FC<{ title: string; url: string }> = (props) => (
       <div className="text-xl font-medium">{props.title}</div>
       <div className="text-xs text-slate-600">{props.url}</div>
     </div>
-    <div>本文采用 CC BY-NC-ND 4.0 协议授权，完整转载请注明出处。</div>
+    <div className="flex flex-col gap-1 text-sm">
+      <div>
+        本文采用 CC BY-NC-SA 4.0 - 非商业性使用 - 相同方式共享 4.0
+        国际进行许可。
+      </div>
+      <div>
+        商业转载请联系站长获得授权，非商业转载请注明本文出处及文章链接，您可以自由地在任何媒体以任何形式复制和分发作品，也可以修改和创作，但是分发衍生作品时必须采用相同的许可协议。
+      </div>
+    </div>
   </div>
 );
 
