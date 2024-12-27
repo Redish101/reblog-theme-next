8a5c5c14144e3810a0bbee503c6caec04a1a738d
Redish101 <i@redish101.top>
2024-12-28 07:48:24 +0800

    fix: 修复了sitemap错误

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,11 +8,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const articles = await themekit.getArticleList({
     pageIndex: 1,
     pageSize: Number.MAX_SAFE_INTEGER,
+    withContent: false,
   });
 
   const articlesSitemap: MetadataRoute.Sitemap =
     articles?.articles.map((article) => ({
       url: `${site?.url!}/article/${article.slug}`,
+      lastModified: new Date(article.updated_at),
       changeFrequency: "daily",
       priority: 0.8,
     })) || [];
