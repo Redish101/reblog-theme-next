e2d84a005a3c2eb71c9c0f8d235558fe5731b77e
Redish101 <i@redish101.top>
2025-02-07 20:42:50 +0800

    fix: 调整 Container 组件的内边距

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Container({ children }: { children: ReactNode }) {
   return (
-    <main className="min-h-screen max-w-[855px] mx-auto px-6 pt-32 break-words">
+    <main className="min-h-screen max-w-[855px] mx-auto px-2 pt-32 break-words">
       {children}
     </main>
   );
